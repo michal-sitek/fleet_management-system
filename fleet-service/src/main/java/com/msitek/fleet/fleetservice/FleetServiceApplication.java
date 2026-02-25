84cdd9fcdceec68143a5dc90c89be960859c086e
Michal Sitek <michal.sitek2306@gmail.com>
2026-02-25 19:06:18 +0100

    chore: initialize fleet and trip services structure

---
+++ fleet-service/src/main/java/com/msitek/fleet/fleetservice/FleetServiceApplication.java
@@ -0,0 +1,13 @@
+package com.msitek.fleet.fleetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FleetServiceApplication.class, args);
+    }
+
+}
---
+++ fleet-service/src/test/java/com/msitek/fleet/fleetservice/FleetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msitek.fleet.fleetservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
